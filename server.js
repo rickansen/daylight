b920e920667de450a0491919834d1c5abab71283
Andersen <rickansenn@gmail.com>
2022-11-15 23:03:04 +0800

    refactor as a notes app

--- server.js
+++ server.js
@@ -1,71 +1,122 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
-
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const PORT = 2121;
+require('dotenv').config();
 
 let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'rap'
+  dbConnectionStr = process.env.DB_STRING,
+  dbName = 'daylight';
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName);
+  }
+);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  db.collection('tasks')
+    .find()
+    .toArray()
+    .then((data) => {
+      response.render('index.ejs', { info: data });
     })
-    
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+    .catch((error) => console.error(error));
+});
 
+app.post('/addPriority', (request, response) => {
+  const Months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const date = new Date();
+  const month = date.getMonth();
+  const day = date.getDay();
+  const year = date.getUTCFullYear();
 
-app.get('/',(request, response)=>{
-    db.collection('rappers').find().sort({likes: -1}).toArray()
-    .then(data => {
-        response.render('index.ejs', { info: data })
+  db.collection('tasks')
+    .insertOne({
+      title: request.body.title,
+      body: request.body.body,
+      date: `${Months[month]} ${day} ${year}`,
     })
-    .catch(error => console.error(error))
-})
-
-app.post('/addRapper', (request, response) => {
-    db.collection('rappers').insertOne({stageName: request.body.stageName,
-    birthName: request.body.birthName, likes: 0})
-    .then(result => {
-        console.log('Rapper Added')
-        response.redirect('/')
+    .then((result) => {
+      console.log('Priority Added');
+      response.redirect('/');
     })
-    .catch(error => console.error(error))
-})
+    .catch((error) => console.error(error));
+});
 
-app.put('/addOneLike', (request, response) => {
-    db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
+app.put('/updatePriority', (request, response) => {
+  const Months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const date = new Date();
+  const month = date.getMonth();
+  const day = date.getDay();
+  const year = date.getUTCFullYear();
+
+  db.collection('tasks')
+    .updateOne(
+      {
+        title: request.body.title,
+        body: request.body.body,
+        date: request.body.date,
+      },
+      {
         $set: {
-            likes:request.body.likesS + 1
-          }
-    },{
-        sort: {_id: -1},
-        upsert: true
+          date: `(Last Edited) ${Months[month]} ${day} ${year}`,
+        },
+      },
+      {
+        sort: { _id: -1 },
+        upsert: true,
+      }
+    )
+    .then((result) => {
+      console.log('Updated');
+      response.json('Updated priority');
     })
-    .then(result => {
-        console.log('Added One Like')
-        response.json('Like Added')
+    .catch((error) => console.error(error));
+});
+
+app.delete('/deletePriority', (request, response) => {
+  db.collection('tasks')
+    .deleteOne({ title: request.body.title })
+    .then((result) => {
+      console.log('Priority Deleted');
+      response.json('Priority Deleted');
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
+});
 
-})
-
-app.delete('/deleteRapper', (request, response) => {
-    db.collection('rappers').deleteOne({stageName: request.body.stageNameS})
-    .then(result => {
-        console.log('Rapper Deleted')
-        response.json('Rapper Deleted')
-    })
-    .catch(error => console.error(error))
-
-})
-
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
